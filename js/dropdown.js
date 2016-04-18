1fa48ca6ddccba07877151392e6878211386be4c
honzi <czechinthemail@gmail.com>
2016-04-17 18:39:16 -0700

    temporarily removed play_audio()

--- js/dropdown.js
+++ js/dropdown.js
@@ -184,16 +184,6 @@ function player_move(){
     }
 }
 
-function play_audio(id){
-    if(document.getElementById('audio-volume').value <= 0){
-        return;
-    }
-
-    document.getElementById(id).volume = document.getElementById('audio-volume').value;
-    document.getElementById(id).currentTime = 0;
-    document.getElementById(id).play();
-}
-
 function reset(){
     if(!window.confirm('Reset settings?')){
         return;
