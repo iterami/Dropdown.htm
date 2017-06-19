82a320b8129688452a00e713019d2017feff09de
honzi <czechinthemail@gmail.com>
2017-06-19 16:51:54 -0700

    removed y-margin

--- js/dropdown.js
+++ js/dropdown.js
@@ -222,14 +222,10 @@ function repo_init(){
         'orange-miss': 1,
         'purple-catch': 1,
         'wrap': 0,
-        'y-margin': 0,
       },
-      'storage-menu': '<table><tr><td><input id=frames-per-purple><td>Frames/Purple_Coin<tr><td><input id=max><td>Max <select id=game-mode><option value=0>Points</option><option value=1>Time</option></select><tr><td><input id=movement-keys maxlength=2><td>Move<tr><td><input id=ms-per-coin-move><td>ms/Coin_Move<tr><td><input id=ms-per-player-move><td>ms/Player_Move<tr><td><select id=orange-miss><option value=0>Disappear</option><option selected value=1>End Game</option><option value=2>Score-1</option></select><td>Orange Coin Miss<tr><td><select id=purple-catch><option value=0>End Game</option><option selected value=1>Score-1</option></select><td>Purple Coin Catch<tr><td><select id=wrap><option value=0>—</option><option value=2>←</option><option value=3>→</option><option value=1>↔</option></select><td>Wrap<tr><td><input id=y-margin><td>Y Margin</table>',
+      'storage-menu': '<table><tr><td><input id=frames-per-purple><td>Frames/Purple_Coin<tr><td><input id=max><td>Max <select id=game-mode><option value=0>Points</option><option value=1>Time</option></select><tr><td><input id=movement-keys maxlength=2><td>Move<tr><td><input id=ms-per-coin-move><td>ms/Coin_Move<tr><td><input id=ms-per-player-move><td>ms/Player_Move<tr><td><select id=orange-miss><option value=0>Disappear</option><option selected value=1>End Game</option><option value=2>Score-1</option></select><td>Orange Coin Miss<tr><td><select id=purple-catch><option value=0>End Game</option><option selected value=1>Score-1</option></select><td>Purple Coin Catch<tr><td><select id=wrap><option value=0>—</option><option value=2>←</option><option value=3>→</option><option value=1>↔</option></select><td>Wrap</table>',
       'title': 'Dropdown.htm',
     });
-
-    // Set margin-top of game-div based on y-margin.
-    document.getElementById('game-div').style.marginTop = core_storage_data['y-margin'] + 'px';
 
     // Setup game div.
     var output = '';
@@ -257,9 +253,6 @@ function repo_init(){
 
 function start(){
     core_storage_save();
-
-    // Set margin-top of game-div based on y-margin.
-    document.getElementById('game-div').style.marginTop = core_storage_data['y-margin'] + 'px';
 
     // Reset colors of buttons.
     var loop_counter = 207;
