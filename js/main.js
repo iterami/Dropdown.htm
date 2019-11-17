b2198078c366c9eeeb926d32144ef87f9f616a60
Jan Hořava <czechinthemail@gmail.com>
2019-11-16 22:35:30 -0800

    moved start() call after core_menu_open check

--- js/main.js
+++ js/main.js
@@ -26,11 +26,10 @@ function repo_init(){
       'menu': true,
       'reset': function(){
           stop();
-          start();
-
           if(core_menu_open){
               core_escape();
           }
+          start();
       },
       'storage': {
         'frames-per-purple': 9,
