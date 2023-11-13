470edac3cf7fd4bc3114e8c0e584c3715f310510
Jan Hořava <czechinthemail@gmail.com>
2023-11-12 18:32:23 -0800

    removed optional option closing tags

--- repo.js
+++ repo.js
@@ -237,9 +237,9 @@ function repo_init(){
         + '<tr><td><input class=mini id=frames-per-purple min=1 step=any type=number><td>Frames/Purple_Coin'
         + '<tr><td><input class=mini id=ms-per-coin-move min=1 step=any type=number><td>ms/Coin_Move'
         + '<tr><td><input class=mini id=ms-per-player-move min=1 step=any type=number><td>ms/Player_Move'
-        + '<tr><td><select id=orange-miss><option value=0>Disappear</option><option selected value=1>End Game</option><option value=2>Score-1</option></select><td>Orange Coin Miss'
-        + '<tr><td><select id=purple-catch><option value=0>End Game</option><option selected value=1>Score-1</option></select><td>Purple Coin Catch'
-        + '<tr><td><select id=wrap><option value=0>No</option><option value=1>Both</option><option value=2>Left</option><option value=3>Right</option></select><td>Wrap</table>',
+        + '<tr><td><select id=orange-miss><option value=0>Disappear<option selected value=1>End Game<option value=2>Score-1</select><td>Orange Coin Miss'
+        + '<tr><td><select id=purple-catch><option value=0>End Game<option selected value=1>Score-1</select><td>Purple Coin Catch'
+        + '<tr><td><select id=wrap><option value=0>No<option value=1>Both<option value=2>Left<option value=3>Right</select><td>Wrap</table>',
       'title': 'Dropdown.htm',
     });
     audio_create({
