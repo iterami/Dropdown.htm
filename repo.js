7ebbd63cc3e1050fdebd3a3f2d10f90e7984153b
Jan Hořava <czechinthemail@gmail.com>
2025-05-27 05:31:34 -0700

    improved pointer movement accuracy

--- repo.js
+++ repo.js
@@ -138,10 +138,11 @@ function player_move(){
     let move_left = core_keys[core_storage_data['move-←']]['state'];
     let move_right = core_keys[core_storage_data['move-→']]['state'];
     if(core_pointer['down-0']){
-        if(core_pointer['x'] > core_elements[195 + player_x].offsetLeft){
+        const position = core_elements[195 + player_x].offsetLeft;
+        if(core_pointer['x'] > position + core_storage_data['width']){
             move_right = true;
 
-        }else{
+        }else if(core_pointer['x'] < position){
             move_left = true;
         }
     }
