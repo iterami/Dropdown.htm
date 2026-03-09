fcd0b802db12036b8a7debf18e619cc1748ed9ca
Jan Hořava <czechinthemail@gmail.com>
2026-03-09 02:42:25 -0700

    skipped extra core_menu_open check

--- repo.js
+++ repo.js
@@ -279,9 +279,7 @@ function start(){
         return;
     }
     reset();
-    if(core_menu_open){
-        core_escape(false);
-    }
+    core_escape(false);
 
     core_interval_modify({
       'id': 'coins',
